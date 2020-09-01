8dc1ce2e05da2134322a39b4d268419502972782
Wojciech Turek <wojciechturek@outlook.com>
2020-09-01 22:17:47 +0200

    change remove

--- src/store/actions/auth.ts
+++ src/store/actions/auth.ts
@@ -25,7 +25,7 @@ export const login = (email: string, password: string) => {
   return (dispatch: any) => {
     dispatch(loginRequest());
     axios.post(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyAYOVrzqWn9mTTpDL2eTaQSKFQssHOG34U"
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=key"
     );
   };
 };
